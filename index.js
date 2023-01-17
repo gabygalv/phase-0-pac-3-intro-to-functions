1f9c299f332e8e8856d525773146bf7e13267cea
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-17 13:42:26 -0700

    following along in intro to functions lesson

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello(){
+    console.log("hello!");
+}
+
+function sayHelloToGuadalupe() {
+console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip");
+}
+
+function sayHelloTo(firstName) {
+console.log(`Hello, ${firstName}!`);
+}
+
+function say(greeting, firstName){
+    console.log(`${greeting}, ${firstName}!`);
+}
+ function add(x, y) {
+    return x + y;
+ }
+function say(greeting, firstName){
+    return(`${greeting}, ${firstName}!`);
+}
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+}
+console.log(say("Howdy", "partner"));
